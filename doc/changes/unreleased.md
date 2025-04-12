b8ba3544df082e10d8986f9c4071bfe491f0d0bd
Torsten Kilias <torsten.kilias@exasol.com>
2025-04-12 13:13:11 +0200

    #7: Add guide how to edit the documentation

--- doc/changes/unreleased.md
+++ doc/changes/unreleased.md
@@ -5,3 +5,4 @@
 * #1: Added initial Project Setup
 * #4; Initial documentation version with outline and first fragments
 * #6: Translate README to rst and split up sections
+* #7: Add guide how to edit the documentation 
--- noxfile.py
+++ noxfile.py
@@ -1,7 +1,14 @@
 import nox
+import os
+import sys
 
 # imports all nox task provided by the toolbox
 from exasol.toolbox.nox.tasks import *
 
 # default actions to be run if nothing is explicitly specified with the -s option
 nox.options.sessions = ["project:fix"]
+
+
+@nox.session(python=False, name="doc:serve")
+def serve_docs(session):
+    session.run(sys.executable, "-m", "http.server", "9000", "--directory", ".html-documentation")
