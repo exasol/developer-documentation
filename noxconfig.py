5ebfbc4580cffc5001204a869a73e258e278ee69
Ariel Schulz <ariel.schulz@exasol.com>
2025-07-02 09:53:05 +0200

    Update exasol-toolbox to 1.6.0

--- noxconfig.py
+++ noxconfig.py
@@ -1,14 +1,15 @@
 from __future__ import annotations
 
+from collections.abc import Iterable
 from dataclasses import dataclass
 from pathlib import Path
-from typing import Iterable
 
 
 @dataclass(frozen=True)
 class Config:
     root: Path = Path(__file__).parent
     doc: Path = Path(__file__).parent / "doc"
+    source: Path = Path("exasol/developer_documentation")
     version_file: Path = (
         Path(__file__).parent / "exasol" / "developer_documentation" / "version.py"
     )
@@ -17,6 +18,7 @@ class Config:
         ".eggs",
         "venv",
     )
+    python_versions = ["3.9", "3.10", "3.11", "3.12", "3.13"]
 
     plugins = []
 
