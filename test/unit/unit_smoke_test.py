f2ac2591d2f83cb28bba2ae5ffecba7cc41e1d85
Ariel Schulz <ariel.schulz@exasol.com>
2025-12-22 08:51:06 +0100

    Add comment explaining F401 usage here

--- test/unit/unit_smoke_test.py
+++ test/unit/unit_smoke_test.py
@@ -1,3 +1,5 @@
+# F401 is used here as this is an import which is not explicitly used. The purpose of
+# the unit_smoke_test.py file is to verify that the Python module can be loaded.
 import exasol.developer_documentation  # noqa: F401
 
 
