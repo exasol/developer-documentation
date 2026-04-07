4e07a4057229e079eeac9ebb05b6ceaa24091d4b
Ariel Schulz <43442541+ArBridgeman@users.noreply.github.com>
2026-04-07 10:13:09 +0200

    Security/50 resolve vulnerabilities april 7th (#51)
    * Use uvx migrate-to-uv to update to be more PEP-compliant
    
    * Update PTB to 6.1.1
    
    * Update workflows to PTB 6.1.1
    
    * Fix pyproject.toml
    
    * Add changelog entry
    
    * Update links
    
    * Update checks with updated noxconfig value for python

--- noxconfig.py
+++ noxconfig.py
@@ -7,7 +7,7 @@ from exasol.toolbox.config import BaseConfig
 PROJECT_CONFIG = BaseConfig(
     root_path=Path(__file__).parent,
     project_name="developer_documentation",
-    python_versions=("3.10", "3.11", "3.12", "3.13", "3.14"),
+    python_versions=("3.10",),
     # The developer-documentation does not run against any Exasol DBs.
     # We put a dummy version here for the purposes of the tests running.
     exasol_versions=("8.29.13",),
